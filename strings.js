81ba1f4d507ce35cbf30f21982d8892f16584e3d
Leo Taylor <leotaylor723@gmail.com>
2018-02-26 20:41:44 -0600

    completed exercise 3

--- strings.js
+++ strings.js
@@ -1,3 +1,13 @@
 // Challnege # 1
 var numOfSandwhiches = 4 * 2;
 console.log("numOfSandwhiches", numOfSandwhiches);
+
+// Challenege #2
+var name = "Leo";
+var string = "Hello, " + name +  " how are you doing today?";
+console.log("string", string);
+
+// Challenge #3
+var DNA ="GCAT"
+var RNA = DNA.replace("T", "U");
+console.log("RNA:", RNA);
